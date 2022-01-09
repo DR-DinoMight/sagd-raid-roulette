70e8ad751341e7f64b81123d5cbf75aebe1ec6b6
Matthew Deloughry <matt@deloughry.co.uk>
2022-01-09 22:12:19 +0000

    :sparkles: Start of the project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['static-cdn.jtvnw.net']
+  },
+  webpack(config, { dev, isServer }) {
+    // ${previousConfig...}
+
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      })
+    }
+
+    return config
+  },
 }
---
+++ pages/api/channel.ts
@@ -0,0 +1,64 @@
+import { request } from 'http';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Data = {
+    user_id: string;
+    user_login: string;
+    user_name: string;
+    game_name: string;
+    title: string;
+    viewer_count: number;
+    started_at: string;
+    thumbnail_url: string;
+    is_mature: boolean;
+    stream_url: string;
+};
+
+//type for request params
+type Params = {
+    category: string;
+    minViewers: string;
+};
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+    const { category = '1469308723' , minViewers = '10' } = req.query as Params;
+
+    //Game Development id = 1469308723
+
+    const requestHeaders: HeadersInit = new Headers();
+    requestHeaders.append('Content-Type', 'application/json');
+    //add Client id
+    requestHeaders.append('Client-ID', process.env.TWITCH_CLIENT_ID ?? '');
+    //add bearer token
+    requestHeaders.append('Authorization', `Bearer ${process.env.TWITCH_ACCESS_TOKEN}`);
+
+    const results = await fetch(
+        `https://api.twitch.tv/helix/streams?game_id=${category}&first=100`,
+        {
+            method: 'GET',
+            headers: requestHeaders,
+        }
+    );
+    const data = await results.json();
+    // Search the data results for a stream with viewer_count less than or equal to minViewers and get a random stream
+
+    let stream : Data = data.data.filter(
+        (stream) => parseInt(stream.viewer_count) <= parseInt(minViewers)
+    )[Math.floor(Math.random() * data.data.length)];
+
+    // If no stream is found, return an error
+    if (!stream) {
+        res.status(404).json({
+            error: 'No stream found',
+        });
+    }
+
+
+    stream.stream_url = `https://player.twitch.tv/?channel=${stream.user_login}&parent=localhost`;
+
+    res.status(200).json(stream);
+}
+
